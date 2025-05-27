766912f4f8ea292032c67e1d1c14fec3c2bb3575
TONY AND SONG <163045349+rocky-ia1@users.noreply.github.com>
2025-05-27 19:16:31 +0100

    Update GroupMetadata.ts

--- src/Types/GroupMetadata.ts
+++ src/Types/GroupMetadata.ts
@@ -1,6 +1,11 @@
 import { Contact } from './Contact'
 
-export type GroupParticipant = (Contact & { isAdmin?: boolean, isSuperAdmin?: boolean, admin?: 'admin' | 'superadmin' | null, phoneNumber?: string | undefined })
+export type GroupParticipant = Contact & {
+    isAdmin?: boolean
+    isSuperAdmin?: boolean
+    admin?: 'admin' | 'superadmin' | null
+    phoneNumber?: string
+}
 
 export type ParticipantAction = 'add' | 'remove' | 'promote' | 'demote' | 'modify'
 
@@ -10,14 +15,11 @@ export type RequestJoinMethod = 'invite_link' | 'linked_group_join' | 'non_admin
 
 export interface GroupMetadata {
     id: string
-    /** group uses 'lid' or 'pn' to send messages */
-    addressingMode: "pn" | "lid"
-    owner: string | undefined
+    addressingMode: 'pn' | 'lid'
+    owner?: string
     subject: string
-    /** group subject owner */
     subjectOwner?: string
     subjectOwnerPhoneNumber?: string
-    /** group subject modification date */
     subjectTime?: number
     creation?: number
     desc?: string
@@ -25,35 +27,24 @@ export interface GroupMetadata {
     descOwnerPhoneNumber?: string
     descId?: string
     descTime?: number
-    /** if this group is part of a community, it returns the jid of the community to which it belongs */
     linkedParent?: string
-    /** is set when the group only allows admins to change group settings */
     restrict?: boolean
-    /** is set when the group only allows admins to write messages */
     announce?: boolean
-    /** is set when the group also allows members to add participants */
     memberAddMode?: boolean
-    /** Request approval to join the group */
     joinApprovalMode?: boolean
-    /** is this a community */
     isCommunity?: boolean
-    /** is this the announce of a community */
     isCommunityAnnounce?: boolean
-    /** number of group participants */
     size?: number
-    // Baileys modified array
     participants: GroupParticipant[]
     ephemeralDuration?: number
     inviteCode?: string
-    /** the person who added you to group or changed some setting in group */
     author?: string
 }
-
 
 export interface WAGroupCreateResponse {
     status: number
     gid?: string
-    participants?: [{ [key: string]: {} }]
+    participants?: Array<{ [key: string]: {} }>
 }
 
 export interface GroupModificationResponse {
